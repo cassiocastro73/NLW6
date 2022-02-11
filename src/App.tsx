4189b4b4fdc82b5ae1c23077e6b1863c63a2242f
Cassio <cassiocastro222@gmail.com>
2022-02-11 15:00:37 -0300

    Inicio do roteamento

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import Home from '../src/pages/Home';
+// import Home from '../src/pages/Home';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import  Home  from './pages/Home';
+import NewRoom from '../src/pages/NewRoom';
+
 
 function App() {
   return (
-    <Home/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/rooms/new-room" element={<NewRoom/>}/>
+        </Routes>
+      </BrowserRouter>
   );
 }
 
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,14 +1,10 @@
-import  { unstable_HistoryRouter }  from 'react-router-dom'; 
-
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
-import googleIconImg from '../assets/images/google-icon.svg';
 
 import '../styles/auth.scss';
 import { Button } from '../components/Button';
 
-export default function Home(){
-
+export default function NewRoom(){
     return(
         <div id= "page-auth">
             <aside>
@@ -19,19 +15,17 @@ export default function Home(){
             <main>
                 <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
-                    <button className='create-room'>
-                        <img src={googleIconImg} alt="Logo do Google" />
-                        Crie sua sala com o Google
-                    </button>
-                    <div className='separator'>ou entre em uma sala</div>
+                    <h2>Crie uma nova sala</h2>
                     <form>
                         <input type="text"
                                 placeholder='Digite o código da sala'
                         />
-
                         <Button type='submit'>
                             Entrar na sala
                         </Button>
+                        <p>
+                            Quer entrar em uma sala existente? <a href="#">Clique aqui</a>
+                        </p>
                     </form>
                 </div>
             </main>
